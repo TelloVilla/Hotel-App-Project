639fde626ff69da90e42d6f14c76c28690dd2fd3
TelloVilla <TelloVilla@protonmail.com>
2021-09-03 10:32:22 -0500

    setup

---
+++ pages/testPage.js
@@ -0,0 +1,12 @@
+import Head from 'next/head'
+import Header from '../components/header'
+
+export default function testPage() {
+  return (
+    <div className="container">
+      <Header/>
+      This is a test page
+      
+    </div>
+  )
+}
