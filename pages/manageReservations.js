638b4e5d6b93ea11d87e879195f8a0a168c123fb
huynhn98 <72099264+huynhn98@users.noreply.github.com>
2021-10-20 10:40:48 -0500

    Add files via upload

---
+++ pages/manageReservations.js
@@ -0,0 +1,14 @@
+export default function manageReservations() {
+    return (
+        <div>
+            <h1>Manage Reservations Here</h1>
+        </div>
+    )
+}
+
+export async function getStaticProps() {
+    
+    return {
+        props: {}
+    }
+}
