5ae3d85759f7e813290192154998337374adfe95
huynhn98 <72099264+huynhn98@users.noreply.github.com>
2021-11-22 12:41:40 -0600

    Update register.js

--- pages/api/register.js
+++ pages/api/register.js
@@ -2,19 +2,13 @@ import {UserDB} from '../../util/user_db'
 const bcrypt = require('bcryptjs')
 
 export default function handler(req, res){
-    let {username, firstname, lastname, password, billingAddr, admin} = req.body;
-    let success = UserDB.createUser(username, firstname, lastname, billingAddr, bcrypt.hashSync(password), admin)
+    console.log(req.body);
+    let {username, firstName, lastName, billingAddr, password, admin} = req.body;
 
-    if(success.password != password) {
-        res.status(401).json({error: "Wrong Password"})
-    }
-
-    if(success.username != username) {
-        res.status(402).json({error: "Wrong Password"})
-    }
-
+    let success = UserDB.createUser(username, firstName, lastName, billingAddr, bcrypt.hashSync(password), admin)
     if(!success){
         res.status(400).json({error: "Invalid User"})
+        return
     }
-    res.status(200).send("Logged out")
+    res.status(200).send("Registered")
 }
