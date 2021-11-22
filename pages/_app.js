232486c1f7c805da904917fcd3f701ca8abe9d3f
huynhn98 <72099264+huynhn98@users.noreply.github.com>
2021-11-22 12:40:22 -0600

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,18 +1,27 @@
 import 'bootstrap/dist/css/bootstrap.css'
 import '../styles/global.sass'
-import header from "../components/header"
-
 import {useEffect} from 'react'
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import * as React from "react"
 
 function MyApp({ Component, pageProps }) {
 
-
+  React.useEffect(() => {
+    toast({ type: "info", message: "Hello world!" });
+  }, []);
   useEffect(()=>{
     import ('bootstrap/dist/js/bootstrap');
   }, [])
 
 
-  return <Component {...pageProps} />
+  return (
+    <>
+  <Component {...pageProps} />
+  <ToastContainer
+      />
+      </>
+  )  
 }
 
 export default MyApp
